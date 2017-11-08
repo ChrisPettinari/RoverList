7c6464f7d0c37f9554a48fed6cbdb365029933a7
ChrisPettinari <chrispettinari@gmail.com>
2017-11-08 12:23:33 -0500

    testing

--- RoverList/Program.cs
+++ RoverList/Program.cs
@@ -11,25 +11,27 @@ namespace RoverList
         static void Main(string[] args)
         {
             // TODO:  Implement the RoverList class
-            // TODO:  Create a RoverList and then fill it with 16 words
             RoverList poop = new RoverList();
+            // TODO:  Create a RoverList and then fill it with 16 words
+
+
 
             // TODO:  Print out the list
 
-            // TODO:  Remove every 3rd word
-            // TODO:  Print out the list
+                // TODO:  Remove every 3rd word
+                // TODO:  Print out the list
+                
+                // TODO:  Prompt the user to input words, add those words to the list until they enter the word "done"
+                // TODO:  Print out the list
 
-            // TODO:  Prompt the user to input words, add those words to the list until they enter the word "done"
-            // TODO:  Print out the list
+                // TODO:  Prompt the user to input words, add those words to the FRONT of the list until they enter the word "done"
+                // TODO:  Print out the list
 
-            // TODO:  Prompt the user to input words, add those words to the FRONT of the list until they enter the word "done"
-            // TODO:  Print out the list
+                // TODO:  Remove every word with an odd length
+                // TODO:  Print out the list
 
-            // TODO:  Remove every word with an odd length
-            // TODO:  Print out the list
-
-            // TODO:  Clear the list
-            // TODO:  Print out the list
+                // TODO:  Clear the list
+                // TODO:  Print out the list
 
 
         }
